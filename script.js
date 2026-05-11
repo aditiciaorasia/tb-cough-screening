578273b79fb1f60889b46ca6f44c63cbb17ed108
aditiciaorasia <143952385+aditiciaorasia@users.noreply.github.com>
2026-05-11 10:51:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -403,3 +403,24 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
 });
+
+/* =========================
+   MOBILE MENU
+========================= */
+
+const menuToggle =
+    document.getElementById("menuToggle");
+
+const mobileMenu =
+    document.getElementById("mobileMenu");
+
+
+if (menuToggle && mobileMenu) {
+
+    menuToggle.addEventListener("click", () => {
+
+        mobileMenu.classList.toggle("active");
+
+    });
+
+}
